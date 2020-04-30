1b1fd2d3699ddb42fdbb65e7f23fbfe6b4d073ae
Leon Lok <leonlok@hotmail.co.uk>
2020-04-30 16:37:04 +0100

    removed unused method

--- camera_server.py
+++ camera_server.py
@@ -2,14 +2,9 @@ from base_camera import BaseCamera
 
 
 class Camera(BaseCamera):
-    video_source = 0
 
     def __init__(self, feed_type, device, image_hub):
         super(Camera, self).__init__(feed_type, device, image_hub)
-
-    @staticmethod
-    def set_video_source(source):
-        Camera.video_source = source
 
     @classmethod
     def server_frames(cls, image_hub):
